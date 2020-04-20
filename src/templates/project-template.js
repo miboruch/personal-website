d3549224bd79e133dff37bc10ae7c28258afbde1
Michał Boruch <miboruch@gmail.com>
2020-04-20 15:00:57 +0200

    Added create pages programmatically

---
+++ src/templates/project-template.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import SEO from '../components/seo';
+import Layout from '../components/templates/Layout';
+import { graphql } from 'gatsby';
+import PortfolioProjectTemplate from '../components/templates/PortfolioProjectTemplate/PortfolioProjectTemplate';
+
+const ProjectTemplate = ({ pageContext: { data }, data: { allFile } }) => {
+  return (
+    <Layout headerTheme='dark'>
+      <>
+        <SEO title={data.name} />
+        <PortfolioProjectTemplate content={data} images={allFile.edges} />
+      </>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ImageQuery($fileDirectory: String!) {
+    allFile(filter: { relativeDirectory: { eq: $fileDirectory } }) {
+      edges {
+        node {
+          ...mockUpFragment
+        }
+      }
+    }
+  }
+`;
+
+export default ProjectTemplate;
